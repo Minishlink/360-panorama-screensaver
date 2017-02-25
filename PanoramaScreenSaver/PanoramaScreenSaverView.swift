af64485701e68894171e6805503796a819d5379f
Louis Lagrange <lagrange.louis@gmail.com>
2017-02-25 21:47:25 +0100

    fix preview and improve performance

--- PanoramaScreenSaver/PanoramaScreenSaverView.swift
+++ PanoramaScreenSaver/PanoramaScreenSaverView.swift
@@ -17,10 +17,16 @@ class PanoramaScreenSaverView: ScreenSaverView {
         
         if (isPreview) {
             self.scaleUnitSquare(to: NSMakeSize(0.25, 0.25))
+            initWebView(frame: CGRect(x: 0, y: 0, width: 1280, height: 800));
+            return;
         }
         
         if (frame.width != 0) {
-            initWebView(frame: frame);
+            let ratioPerformance = CGFloat(1.25);
+            let newFrame = CGRect(x: 0, y: 0, width: frame.width/ratioPerformance, height: frame.height/ratioPerformance);
+            // alternate monitors have a frame where the origin is not set to 0 by default
+            self.scaleUnitSquare(to: NSMakeSize(ratioPerformance, ratioPerformance))
+            initWebView(frame: newFrame);
         }
     }
     
@@ -33,7 +39,7 @@ class PanoramaScreenSaverView: ScreenSaverView {
         self.addSubview(webView)
         
         // TODO investigate websiteDataStore to store pictures
-
+        
         let urlpath = Bundle(for: type(of: self)).resourcePath! + "/PanoramaWebView/index.html";
         let url = URL(fileURLWithPath: urlpath)
         webView.load(URLRequest(url: url))
